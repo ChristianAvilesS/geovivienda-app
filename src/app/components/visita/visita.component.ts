8853aa7f641976982ca908c098fcd446be016436
FabriAL05 <fabrizioafv@gmail.com>
2025-06-23 18:10:17 -0500

    Creación de Comentario y Visita

---
+++ src/app/components/visita/visita.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-visita',
+  imports: [],
+  templateUrl: './visita.component.html',
+  styleUrl: './visita.component.css'
+})
+export class VisitaComponent {
+
+}
