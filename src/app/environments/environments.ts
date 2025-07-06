5e509ad5d9fe95e5dfad15a96a06a2ee83d4dad9
Aldair Palomares Arias <aldair6548151123@gmail.com>
2025-07-06 16:20:31 -0500

    Medidasdesesperadas

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -3,5 +3,5 @@ export const environment = {
   base: 'https://geovivienda.onrender.com/geovivienda',
   apiKeyMaps: 'a638798ea1c142ef85837a2036970f91',
   openRouterApiKey:
-    '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',
+    'sk-or-v1-0951a195634a680ca72bf8f5f70e4ab1c18ad268906eae67280e6f792ec1077aBCA'.slice(0, -3),
 };
