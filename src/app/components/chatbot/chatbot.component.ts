2e9fbcaca0ef2d9677e5851bfaf9eab3bd0b24d0
Piwui <aldair6548151123@gmail.com>
2025-07-06 14:01:48 -0500

    ULTIMO DETALLE API KEY Se oculto la api en Environments.ts y se agrego la api de openrouter para poder hacer el deploy y que no se desactive la api key de openrouter

--- src/app/components/chatbot/chatbot.component.ts
+++ src/app/components/chatbot/chatbot.component.ts
@@ -5,11 +5,20 @@ import { HttpClient } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { environment } from '../../environments/environments';
 
 @Component({
   selector: 'app-chatbot',
   standalone: true,
-  imports: [CommonModule, FormsModule, NgIf, NgFor, NgClass, MatButtonModule, MatIconModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    NgIf,
+    NgFor,
+    NgClass,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './chatbot.component.html',
   styleUrls: ['./chatbot.component.css'],
 })
@@ -41,7 +50,7 @@ export class ChatbotComponent {
         {
           role: 'user',
           content:
-            'Eres un asistente útil de Geoviviendas una aplicacion web que sirve para buscar casas o departamentos para alquilarlar o venderlas si eres un ARRENDATARIO o VENDEDOR y como COMPRADOR poder alquilar o comorar, es una app web completa que permite valorar inmuebles, editar el usuario y los inmuebles, login y cuenta con un mapa donde encontrar los inmuebles. Solo responde preguntas sobre la aplicación. No des información adicional. Si no sabes la respuesta, di "No deseo joven, gracias.".',
+            'Eres un asistente útil de Geoviviendas una aplicacion web que sirve para buscar casas o departamentos para alquilarlar o venderlas si eres un ARRENDATARIO o VENDEDOR y como COMPRADOR poder alquilar o comprar, es una app web completa que permite valorar inmuebles, editar el usuario y los inmuebles, login y cuenta con un mapa donde encontrar los inmuebles. Solo responde preguntas sobre la aplicación. No des información adicional. Si no sabes la respuesta, di "Ahora no joven, gracias. Cada que recivas un hola o saludo empiezas desde cero".',
         },
         ...this.messages.map((msg) => ({
           role: msg.sender === 'user' ? 'user' : 'assistant',
@@ -55,7 +64,7 @@ export class ChatbotComponent {
         this.http.post('https://openrouter.ai/api/v1/chat/completions', body, {
           headers: {
             'Content-Type': 'application/json',
-            Authorization: `Bearer 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`,
+            Authorization: `Bearer ${environment.openRouterApiKey}`,
             'HTTP-Referer': 'https://localhost:4200/',
             'X-Title': 'geovivienda-app',
           },
